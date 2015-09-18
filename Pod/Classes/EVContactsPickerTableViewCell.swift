94a3a444ae489032dbf64bfc1b1885b9f5d1bb0f
Edward Valentini <edward@interlook.com>
2015-09-18 01:44:33 -0700

    create more files

---
+++ Pod/Classes/EVContactsPickerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EVContactsPickerTableViewCell.swift
+//  Pods
+//
+//  Created by Edward Valentini on 9/18/15.
+//
+//
+
+import UIKit
+
+class EVContactsPickerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
