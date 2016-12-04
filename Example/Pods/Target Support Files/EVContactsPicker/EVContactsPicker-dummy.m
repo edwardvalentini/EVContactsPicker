fb3f55c20bee4d26929e4f13dc5fb9d472843f3b
Edward Valentini <edward@interlook.com>
2016-12-04 15:04:03 -0800

    spring cleaning

--- Example/Pods/Target Support Files/EVContactsPicker/EVContactsPicker-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_EVContactsPicker : NSObject
-@end
-@implementation PodsDummy_EVContactsPicker
-@end
--- Example/Pods/Target Support Files/Pods-EVContactsPicker_Example/Pods-EVContactsPicker_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EVContactsPicker_Example : NSObject
-@end
-@implementation PodsDummy_Pods_EVContactsPicker_Example
-@end
